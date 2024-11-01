3a579e2a0d356cb4adb6457497012306eed3f58a
Zekiye Öztürk <zekiyeerbas35@gmail.com>
2024-11-01 06:08:20 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -9,17 +9,17 @@ module.exports = defineConfig({
   experimentalStudio: true,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    overwrite: true, //deger true olursa eski html rapor dosyasını silip yeniden olusturur, true olursa index001 seklinde diğer dosyaları olusturur
-    charts: true, // Testlerin pass ve fail durumlarını grafiksel olarak raporlar
-    reportPageTitle: 'ToolShop Project Report', // Rapor başlığının custumise edilmesi için kullanılır
-    reportFilename: "[status]_[datetime]-[name]-report", //raporlama dosyasının isimlendirmesi için kullanılır fail_08.02.2024.Perşembe-RegisterTest-report.html
-    timestamp: "shortDate",//dosya ismindeki tarih alanının düzenlenmesinde kullanılır. shortDate, longDate seklinde kullanılır
-    embeddedScreenshots: true, // gömülü ekran görüntüsünü harici bir dosyaya değil direct olarak html rapor dosyasına ekler
-    inlineAssets: true, //assets klasorünün html dosyalarını html dosyasına gömülerek eklenmesini saglar
-    saveAllAttempts: false,// fail olan testlerde eğer test tekrarı yapıyorsak sadece son teste ait ekran görüntüsünü rapora ekler
-    ignoreVideos: false, // true ise testlerin video dosyalarını ignore eder
-    videoOnFailOnly: false //true ise sadece fail durumlarında videoyu ekle-false ise tum testlerde videoyu ekle
-    //videosOnFailOnly: true/false     komutunun calisabilmesi icin ignoreVideos: false olmasi gerekiyor
+    overwrite: true, 
+    charts: true, 
+    reportPageTitle: 'QATEST Project Report', 
+    reportFilename: "[status]_[datetime]-[name]-report", 
+    timestamp: "shortDate",
+    embeddedScreenshots: true, 
+    inlineAssets: true, 
+    saveAllAttempts: false,
+    ignoreVideos: false, 
+    videoOnFailOnly: false 
+  
   },
   screenshotOnRunFailure: true, // Test fail olduğu durumda ekran dörüntüsü alır (npx cypress run komutu ile test çalıştırıldığında)
   trashAssetsBeforeRuns: true, // Test tekrar çalıştırıldığında önceki testten kalan resim ve videoları siler
